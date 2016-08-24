17c00ddc6a500bbcfb5f4eb16d29058eb5139b73
VidyaMoorthy <vidyamoorthy1388@gmail.com>
2016-08-24 17:31:57 +0200

    Update testDB.py

--- testDB.py
+++ testDB.py
@@ -12,18 +12,14 @@ def main():
 		print "I am unable to connect to the database"
 
 	cur = con.cursor()
-	cur.execute("CREATE TABLE orcidprofileTrail (pmid integer , orcidid varchar, firstname varchar,lastname varchar, othername varchar);")
 	with open('/home/vidya/Desktop/OntoForce/data/utils/old/trail.text') as f:
 		content = f.readlines()
 		for eachline in content:
-			#pmid ,orcidId, first_name, last_name,other_name = re.findall("\[(.*?)\]", eachline)
+		
 			pmid ,orcidId, first_name, last_name,other_name = eachline.split("\t")
-
-			cur.execute("INSERT INTO orcidprofileTrail (pmid, orcidid,firstname,lastname,othername) VALUES (%s,%s,%s,%s,%s)", (pmid,orcidId,first_name, last_name,other_name ))
-			#cur.execute("INSERT INTO orcidprofileTrail VALUES (pmid,orcidId,first_name, last_name,other_name )")
+			cur.execute("INSERT INTO orcidprofile (pmid, orcidid,firstname,lastname,othername) VALUES (%s,%s,%s,%s,%s)", (pmid,orcidId,first_name, last_name,other_name ))
+			
 			con.commit()
-			sys.exit
-
+		print "complete"
 			
 main()
-
